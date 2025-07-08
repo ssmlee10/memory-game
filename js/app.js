8eab0a55aab2f0cb0736d79338d6c069c413ee4d
Sharon Lee <ssmlee10@gmail.com>
2025-07-08 07:53:37 -0700

    add pseudocode

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 
 
 /*---------- Variables (state) ---------*/
+// setup board
+// shuffle cards
 let cardValues = [
     '🙃', '🙃',
     '😍', '😍',
@@ -18,7 +20,17 @@ let cardValues = [
 
 
 /*-------------- Functions -------------*/
+// render game board
 
+// check for a match
+
+// check if all cards have been matched (you win!
+
+// restart game
+
+// add a timer
+
+// if timer runs out, stop game, and display try again message
 
 /*----------- Event Listeners ----------*/
-
+// listen for card click, flip over when card has been clicked
