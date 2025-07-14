4360a8da6feb8829592abcd6031d8affb518b782
Sharon Lee <ssmlee10@gmail.com>
2025-07-14 11:54:03 -0700

    change source for gameplay button to github link

--- js/app.js
+++ js/app.js
@@ -10,6 +10,6 @@ enterBtn.addEventListener("click", () => {
   clickSound.currentTime = 0;
   clickSound.play();
   setTimeout(() => {
-    window.location.href = "pages/gamePage.html";
+    window.location.href = "https://ssmlee10.github.io/memory-game/pages/gamePage.html";
   }, 300);
 });
