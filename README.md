ed1a7dd8e10ef97411103381e6da04859d66f742
Sharon Lee <ssmlee10@gmail.com>
2025-07-14 00:30:10 -0700

    update readme

--- README.md
+++ README.md
@@ -1,9 +1,47 @@
-# 🧺 Match the Missing Sock Pairs 🧦
+# 🧺 Match the Sock Pairs Memory Game 🧦
+
+![game logo](images/laundryCard.png)
 
 Match the Missing Sock Pairs is a cozy game that involves the player helping Patty match her socks so they don't go missing!
 
 Patty has experienced so many socks get lost in her laundry machine, in her hamper, and practically all over her house! 
 
-The player has to match 8 pairs of socks in 50 moves or less so that her socks don't go missing.
+The player has to match 8 pairs of socks in 50 moves or less otherwise, there's a chance her socks will go missing!
 
-[Link to play here!](https://ssmlee10.github.io/memory-game/)
+[Link to play the game!](https://ssmlee10.github.io/memory-game/)
+
+## Game Instructions
+
+1. Click on a card to flip it over.
+2. Flip another card to check if it's a match.
+3. If not, they'll flip back.
+4. Matched pairs will not be clickable once they are found. They will also be less opaque.
+5. Match all 8 pairs of socks in 50 moves or less to win!
+
+## Getting Started
+[Pseudocode & Planning](https://docs.google.com/document/d/1_WWRMNrkBkRLAMoWdcRns5wdnnlk12sz-SnJ3mMuA3M/edit?usp=sharing)
+
+Here's a look at how the planning process started. I began by brainstorming what users would want to see and experience if they played a memory game. Then, I turned these stories into pseudocode. Doing this process helped me understand and create a game plan and create actionable steps on how I could translate what I wanted into Javascript language.
+
+## Attributions
+
+[W3 Schools Animated Button](https://www.w3schools.com/cSS/tryit.asp?filename=trycss_buttons_animate3):
+This resource was used to create a hover state and animation when button was clicked
+
+## Technologies Used
+* HTML
+* CSS
+* Javascript
+
+## Next Steps
+* 3 Levels of difficulty
+    * Easy: current game
+    * Medium: add a 1 minute timer to complete
+    * Hard: Increase socks grid to 5x5
+
+* Shuffle cards function
+    * When the reset button is clicked, cards will be shuffled so it will have a shuffled board
+
+* Style
+    * When hovering the mouse over each card, display a shadow under the card
+    * Show a flipping card effect when each of the cards are clicked and clicked back
--- js/app.js
+++ js/app.js
@@ -15,120 +15,126 @@ let win = false;
 let lose;
 
 /*----- Cached Element References  -----*/
-const messageElement = document.querySelector('#messageElement');
-const boardElements = document.querySelector('#board');
-const cardElements = document.querySelectorAll('.card');
-const resetBtn = document.querySelector('#reset');
-const audio = document.getElementById('audio');
+const messageElement = document.querySelector("#messageElement");
+const boardElements = document.querySelector("#board");
+const cardElements = document.querySelectorAll(".card");
+const resetBtn = document.querySelector("#reset");
+const audio = document.getElementById("audio");
 
 /*-------------- Functions -------------*/
 // initial board for reset
 function init() {
-    firstCardClicked = undefined;
-    secondCardClicked = undefined;
-    matchedPairs = 0;
-    tries = 0;
-    triesLeft = 50 - tries;
-};
+  firstCardClicked = undefined;
+  secondCardClicked = undefined;
+  matchedPairs = 0;
+  tries = 0;
+  triesLeft = 50 - tries;
+}
 
 // register click values
 function handleClick(event) {
-    // lose function
-    // end game if 8 pairs not found in 50 tries
-    if (tries === 50) {
-        return;
-        updateMessage();
-    }
-    // win, end game
-    if (matchedPairs === 8) {
-        return;
-    }
-    countTries();
-    const parentElement = event.target.parentElement;
-    if (firstCardClicked === undefined) {
-        firstCardClicked = parentElement;
-        console.log(firstCardClicked.getAttribute("class"));
-        console.log(tries);
-        firstCardClicked.children[0].classList.add('toggleImg');
-        firstCardClicked.children[1].classList.remove('toggleImg');
-    } else {
-        secondCardClicked = parentElement;
-        console.log(secondCardClicked.classList);
-        secondCardClicked.children[0].classList.add('toggleImg');
-        secondCardClicked.children[1].classList.remove('toggleImg');
-        let firstCardClass = firstCardClicked.getAttribute("class");
-        let secondCardClass = secondCardClicked.getAttribute("class");
-        flipCardBack();
-        checkWin();
-        checkMatch();
-    }
-};
+  // lose function
+  // end game if 8 pairs not found in 50 tries
+  if (tries === 50) {
+    return;
+    updateMessage();
+  }
+  // win, end game
+  if (matchedPairs === 8) {
+    return;
+  }
+  countTries();
+  const parentElement = event.target.parentElement;
+  if (firstCardClicked === undefined) {
+    firstCardClicked = parentElement;
+    console.log(firstCardClicked.getAttribute("class"));
+    console.log(tries);
+    firstCardClicked.children[0].classList.add("toggleImg");
+    firstCardClicked.children[1].classList.remove("toggleImg");
+  } else {
+    secondCardClicked = parentElement;
+    console.log(secondCardClicked.classList);
+    secondCardClicked.children[0].classList.add("toggleImg");
+    secondCardClicked.children[1].classList.remove("toggleImg");
+    let firstCardClass = firstCardClicked.getAttribute("class");
+    let secondCardClass = secondCardClicked.getAttribute("class");
+    flipCardBack();
+    checkWin();
+    checkMatch();
+  }
+}
 
 // count number of tries
 // updateMessage to show tries used, tries left
 function countTries() {
-    tries = tries + 1;
-    triesLeft = 50 - tries;
-    updateMessage();
-};
+  tries = tries + 1;
+  triesLeft = 50 - tries;
+  updateMessage();
+}
 
 // check for a match
 function checkMatch() {
-    if (firstCardClicked.getAttribute("class") === secondCardClicked.getAttribute("class")) {
-        matchedPairs = matchedPairs + 1;
-        updateMessage();
-        firstCardClicked.classList.add('matched');
-        secondCardClicked.classList.add('matched');
-        // add class to cards that match (save this for the end)
-        // somehow do something with the class, can't be clicked on, so same pairs are not clicked again
-        // cards flipped over, then won't be able to click again
-    }
+  if (
+    firstCardClicked.getAttribute("class") ===
+    secondCardClicked.getAttribute("class")
+  ) {
+    matchedPairs = matchedPairs + 1;
+    updateMessage();
+    firstCardClicked.classList.add("matched");
+    secondCardClicked.classList.add("matched");
+    // add class to cards that match (save this for the end)
+    // somehow do something with the class, can't be clicked on, so same pairs are not clicked again
+    // cards flipped over, then won't be able to click again
+  }
 
-    resetAfterMatch();
-};
+  resetAfterMatch();
+}
 
 // if match is not found, flip back the cards that weren't a match
 function flipCardBack() {
-    if (firstCardClicked.getAttribute("class") != secondCardClicked.getAttribute("class")) {
-        console.log(firstCardClicked.children[0]);
-        console.log(secondCardClicked);
-        const firstFront = firstCardClicked.children[0];
-        const firstBack = firstCardClicked.children[1];
-        const secondFront = secondCardClicked.children[0];
-        const secondBack = secondCardClicked.children[1];
-        setTimeout(() => {
-            firstFront.classList.remove('toggleImg');
-            firstBack.classList.add('toggleImg');
-            secondFront.classList.remove('toggleImg');
-            secondBack.classList.add('toggleImg');
-        }, 1000);
-    }
+  if (
+    firstCardClicked.getAttribute("class") !=
+    secondCardClicked.getAttribute("class")
+  ) {
+    console.log(firstCardClicked.children[0]);
+    console.log(secondCardClicked);
+    const firstFront = firstCardClicked.children[0];
+    const firstBack = firstCardClicked.children[1];
+    const secondFront = secondCardClicked.children[0];
+    const secondBack = secondCardClicked.children[1];
+    setTimeout(() => {
+      firstFront.classList.remove("toggleImg");
+      firstBack.classList.add("toggleImg");
+      secondFront.classList.remove("toggleImg");
+      secondBack.classList.add("toggleImg");
+    }, 1000);
+  }
 }
 
 // after a match pair is found, reset to find another match
 function resetAfterMatch() {
-    firstCardClicked = undefined;
-    secondCardClicked = undefined;
-};
+  firstCardClicked = undefined;
+  secondCardClicked = undefined;
+}
 
 // udpateMessage showing how many pairs found thus far
 function updateMessage() {
-    if (tries === 50) {
-        messageElement.textContent = `You matched ${matchedPairs} out of 8 sock pairs! :( Hit reset and try again!`
-    } else if (tries >= 0) {
-        messageElement.textContent = `You have ${matchedPairs} matched pairs in ${tries} moves! Moves left: ${triesLeft}`
-    }
-    if (matchedPairs === 8) {
-        messageElement.textContent = `Impressive! You found all 8 pairs in ${tries} moves!\nThank you for being our sock-matching hero!! #teamnomorelonelysocks`
-    }
-};
+  if (tries === 50) {
+    messageElement.textContent = `You matched ${matchedPairs} out of 8 sock pairs! :( Hit reset and try again!`;
+  } else if (tries >= 0) {
+    messageElement.textContent = `You have ${matchedPairs} matched pairs in ${tries} moves!\nMoves left: ${triesLeft}`;
+  }
+  if (matchedPairs === 8) {
+    messageElement.textContent = `Impressive! You found all 8 pairs in ${tries} moves!\nThank you for being our sock-matching hero!! #teamnomorelonelysocks`;
+  }
+}
 
 //winning function
 function checkWin() {
-    if (matchedPairs === 8) {
-        win = true;
-        console.log(win);
-    }
+  if (matchedPairs === 8) {
+    win = true;
+    console.log(win);
+  }
 }
 
 // /*----------- Set Initial Audio Volume ----------*/
@@ -140,13 +146,13 @@ init();
 // this functions the same way as clicking page refresh
 // assistance from Glenn
 const reload = () => {
-    window.location.reload();
-}
+  window.location.reload();
+};
 
 /*----------- Event Listeners ----------*/
 // listen for card click, flip over when card has been clicked
 cardElements.forEach((cardElement) => {
-    cardElement.addEventListener('click', handleClick);
+  cardElement.addEventListener("click", handleClick);
 });
 
-resetBtn.addEventListener('click', reload);
+resetBtn.addEventListener("click", reload);
